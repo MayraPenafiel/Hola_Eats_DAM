c3bf8f59f6b226efaeee665f02ca6476a1fcd2a2
EdwinFabricio <81194080+EdwinFabricio@users.noreply.github.com>
2022-04-08 23:58:46 -0500

    Registro y Login Listos. psdt; pilas con el nombre de sus metodos post en la API y el link del localhos

--- app/src/main/java/com/proyecto/holaeats/api/Apis.java
+++ app/src/main/java/com/proyecto/holaeats/api/Apis.java
@@ -1,4 +1,20 @@
 package com.proyecto.holaeats.api;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Apis {
+
+    private static HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    public static Retrofit getInstance() {
+        Retrofit.Builder builder = new Retrofit.Builder().baseUrl("http://192.168.0.104:8080") //revisar y cambiar al ip de tu localhost
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build());
+        return builder.build();
+
+    }
+
+
 }
--- app/src/main/java/com/proyecto/holaeats/api/ServicePersona.java
+++ app/src/main/java/com/proyecto/holaeats/api/ServicePersona.java
@@ -2,14 +2,31 @@ package com.proyecto.holaeats.api;
 
 import java.util.List;
 import com.proyecto.holaeats.modelo.Persona;
+import com.proyecto.holaeats.modelo.ResponseClass;
 
-//import retrofit2.Call;
-//import retrofit2.http.Body;
-//import retrofit2.http.GET;
-//import retrofit2.http.POST;
-//import retrofit2.http.Path;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ServicePersona {
     //@GET("listar")
     //Call<List<Persona>> getPersonas();
+
+    @PUT("/addUser")
+    Call<ResponseClass> addUser(@Body Persona responseRegisterClass);
+
+
+    @POST("/getUser")
+    Call<ResponseClass> getUser(@Body Persona responseRegisterClass);
 }
+
+
+
+
+
+
