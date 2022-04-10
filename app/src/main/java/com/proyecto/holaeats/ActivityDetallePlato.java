47bd17c293cc86122bb9a2ae7de9534c7bffd773
ABYGAIL1410 <daysizhingri@gmail.com>
2022-04-10 18:34:11 -0500

    Detalle de Plato

---
+++ app/src/main/java/com/proyecto/holaeats/ActivityDetallePlato.java
@@ -0,0 +1,17 @@
+package com.proyecto.holaeats;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityDetallePlato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_plato);
+    }
+    private void getPlatos(){
+
+    }
+}
