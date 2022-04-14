c8feceac672d8e676fea9125147c658420065410
cindycedillo <cindy.cedillo.est@tecazuay.edu.ec>
2022-04-13 21:20:14 -0500

    Se agregó barra de navegación

--- app/src/main/java/com/proyecto/holaeats/ActivityHome.java
+++ app/src/main/java/com/proyecto/holaeats/ActivityHome.java
@@ -9,17 +9,17 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HistorialPedidosActivity extends AppCompatActivity {
+public class ActivityHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_historial_pedidos);
+        setContentView(R.layout.activity_home);
+
         BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
-        navigation.setSelectedItemId(R.id.history);
+        navigation.setSelectedItemId(R.id.home);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
 
     private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -27,11 +27,11 @@ public class HistorialPedidosActivity extends AppCompatActivity {
             switch (item.getItemId()){
 
                 case R.id.home:
-                    startActivity(new Intent(getApplicationContext()
-                            ,ActivityHome.class));
-                    overridePendingTransition(0,0);
                     return true;
                 case R.id.history:
+                    startActivity(new Intent(getApplicationContext()
+                            ,HistorialPedidosActivity.class));
+                    overridePendingTransition(0,0);
                     return true;
 
 
