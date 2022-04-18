5c4b4c0bdfd429e75ad26fd54152cdabd9ba9df6
cindycedillo <cindy.cedillo.est@tecazuay.edu.ec>
2022-04-18 17:36:56 -0500

    Barra de navegación arreglada, modificacion activity cuenta y perfil

--- app/src/main/java/com/proyecto/holaeats/ActivityCuentayPerfl.java
+++ app/src/main/java/com/proyecto/holaeats/ActivityCuentayPerfl.java
@@ -1,8 +1,13 @@
 package com.proyecto.holaeats;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ActivityCuentayPerfl extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class ActivityCuentayPerfl extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interfaz_cuentay_perfil);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setSelectedItemId(R.id.Cuenta);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    startActivity(new Intent(getApplicationContext()
+                            ,ActivityMainPlatos.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.history:
+                    startActivity(new Intent(getApplicationContext()
+                            ,HistorialPedidosActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.Cuenta:
+                    return true;
+            }
+            return false;
+        }
+    };
 }
